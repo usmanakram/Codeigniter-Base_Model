e075803fe6ebe80065bc36fa30804fa79836245c
Usman Akram <usman.akram99@hotmail.com>
2016-10-25 11:17:18 +0500

    Replace function ByID to ByPK

--- core/Base_Model.php
+++ core/Base_Model.php
@@ -4,14 +4,33 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Base_Model extends CI_Model
 {
 	private $table;
+	private $primary_key;
 
 	/**
-	 * $table: optional (if database table name and model name are same. eg: database.user & user_model)
+	 * $table: (String) (optional, if database table name and model name are same. eg: database.user & user_model)
+	 * $primary_key: (String) (optional)
 	 */
-	public function __construct($table = NULL) {
+	public function __construct($table = NULL, $primary_key = NULL) {
 		parent::__construct();
+		
 		$this->setTableName($table);
+		$this->setPrimaryKey($primary_key);
 	}
+
+	/*public function setDbName()
+	{
+		$this->db_name = $this->db->query('SELECT DATABASE() AS db_name')->row()->db_name;
+	}
+
+	public function getTableNames()
+	{
+		$this->setDbName();
+		
+		$this->db->select('table_name');
+		$this->db->where('table_schema', $this->db_name);
+		$query = $this->db->get('information_schema.tables');
+		return $query->result_array();
+	}*/
 
 	/**
 	 * Set the table name
@@ -22,6 +41,18 @@ class Base_Model extends CI_Model
 			$this->table = preg_replace('/(_m|_model)?$/', '', strtolower(get_class($this)));
 		} else {
 			$this->table = $table;
+		}
+	}
+
+	/**
+	 * Set the primary key for current table
+	 */
+	private function setPrimaryKey($primary_key)
+	{
+		if($primary_key == NULL) {
+			$this->primary_key = $this->db->query("SHOW KEYS FROM `" . $this->table . "` WHERE Key_name = 'PRIMARY'")->row()->Column_name;
+		} else {
+			$this->primary_key = $primary_key;
 		}
 	}
 
@@ -45,9 +76,9 @@ class Base_Model extends CI_Model
 		return $query->result_array();
 	}
 
-	public function getByID($id)
+	public function getByPK($id)
 	{
-		return $this->get_record(array('id' => $id));
+		return $this->get_record( array($this->primary_key => $id) );
 	}
 
 	public function getByFieldName($name, $value)
@@ -69,9 +100,9 @@ class Base_Model extends CI_Model
 		return $this->db->update($this->table, $data, $where);
 	}
 
-	public function updateByID($data, $id)
+	public function updateByPK($data, $id)
 	{
-		return $this->update($data, array('id' => $id));
+		return $this->update($data, array($this->primary_key => $id));
 	}
 
 	public function delete($where)
@@ -79,8 +110,15 @@ class Base_Model extends CI_Model
 		return $this->db->delete($this->table, $where);
 	}
 
-	public function deleteByID($id)
+	public function deleteByPK($id)
 	{
-		return $this->delete(array('id' => $id));
+		return $this->delete( array($this->primary_key => $id) );
+	}
+
+	public function incrementByPK($field, $id)
+	{
+		$this->db->where($this->primary_key, $id);
+		$this->db->set($field, $field . '+1', FALSE);
+		return $this->db->update($this->table);
 	}
 }
